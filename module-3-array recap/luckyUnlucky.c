c289f58c96094a781283f53675e7df8046ec4fc2
SohelShahid117 <sohelshahid09@gmail.com>
2024-10-27 12:51:09 +0600

    done 3.3 Lucky Array

--- module-3-array recap/luckyUnlucky.c
+++ module-3-array recap/luckyUnlucky.c
@@ -1,8 +1,56 @@
 #include <stdio.h>
 int main()
 {
-    printf("hi\n");
-    // https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/J
+    // printf("hi\n");
+    //  https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/J
+
+    int n;
+    scanf("%d", &n);
+
+    int A[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &A[i]);
+    }
+
+    // printf("\n");
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     printf("%d ", A[i]);
+    // }
+
+    int min = A[0];
+
+    for (int i = 0; i < n; i++)
+    {
+        if (A[i] <= min)
+        {
+            min = A[i];
+        }
+    }
+    // printf("\n%d\n", min);
+
+    int luck = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (A[i] == min)
+        {
+            luck++;
+        }
+    }
+
+    // printf("\nlucky number is :%d\n", luck);
+
+    if (luck % 2 == 0)
+    {
+        printf("Unlucky");
+    }
+    else
+    {
+        printf("Lucky");
+    }
 
     return 0;
 }
